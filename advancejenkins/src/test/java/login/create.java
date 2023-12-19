2d54ce89ff1889f3d0e5a2cf4ba2eeb6ca25308c
Dhanush K V <Dhanush K V@TYSS-Dhanush-K-V>
2023-12-19 12:41:20 +0530

    second

---
+++ advancejenkins/src/test/java/login/create.java
@@ -0,0 +1,13 @@
+package login;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class create {
+	@Test
+	public void web()
+	{
+		ChromeDriver driver=new ChromeDriver();
+	}
+
+}
